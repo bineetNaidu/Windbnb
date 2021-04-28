0b43f947d3c8478639b42a384a1522e8f6ccd647
bineetNaidu <bineet.s.naidu@gmail.com>
2021-04-28 20:28:10 +0530

    🎉 add css

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,17 @@
 import { Header } from './Header';
+import './app.css';
 
 export function App() {
   return (
-    <div className="app" style={{ margin: '2rem 10%' }}>
+    <div className="MainApp">
       <Header />
+
+      <section className="container">
+        <article className="container__header">
+          <span>Stay in Finland</span>
+          <span>12+ stays</span>
+        </article>
+      </section>
     </div>
   );
 }
