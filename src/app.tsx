1d2f2d2a79f648e2a4c4227e51a346da055556c1
bineetNaidu <bineet.s.naidu@gmail.com>
2021-04-28 21:03:17 +0530

    🎉 add base room component

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import { Header } from './Header';
+import { Room } from './Room';
 import './app.css';
 
 export function App() {
@@ -11,6 +12,57 @@ export function App() {
           <span>Stay in Finland</span>
           <span>12+ stays</span>
         </article>
+      </section>
+
+      <section className="row">
+        <Room
+          img_url="https://images.unsplash.com/photo-1615876234886-fd9a39fda97f?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfFJfRnluLUd3dGx3fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+          rating={5}
+          room_desc="Feels Like Summer in the Air"
+          isSuperhost
+          number_of_beds={2}
+          room_details={'private room'}
+        />
+        <Room
+          img_url="https://images.unsplash.com/photo-1615876234886-fd9a39fda97f?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfFJfRnluLUd3dGx3fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+          rating={5}
+          room_desc="Feels Like Summer in the Air"
+          isSuperhost
+          number_of_beds={2}
+          room_details={'private room'}
+        />
+        <Room
+          img_url="https://images.unsplash.com/photo-1615876234886-fd9a39fda97f?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfFJfRnluLUd3dGx3fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+          rating={5}
+          room_desc="Feels Like Summer in the Air"
+          isSuperhost
+          number_of_beds={2}
+          room_details={'private room'}
+        />
+        <Room
+          img_url="https://images.unsplash.com/photo-1615876234886-fd9a39fda97f?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfFJfRnluLUd3dGx3fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+          rating={5}
+          room_desc="Feels Like Summer in the Air"
+          isSuperhost
+          number_of_beds={2}
+          room_details={'private room'}
+        />
+        <Room
+          img_url="https://images.unsplash.com/photo-1615876234886-fd9a39fda97f?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfFJfRnluLUd3dGx3fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+          rating={5}
+          room_desc="Feels Like Summer in the Air"
+          isSuperhost
+          number_of_beds={2}
+          room_details={'private room'}
+        />
+        <Room
+          img_url="https://images.unsplash.com/photo-1615876234886-fd9a39fda97f?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfFJfRnluLUd3dGx3fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+          rating={5}
+          room_desc="Feels Like Summer in the Air"
+          isSuperhost
+          number_of_beds={2}
+          room_details={'private room'}
+        />
       </section>
     </div>
   );
