ce7c6bc22ceb0185a22b24582c8c1edbc5e867a4
bineetNaidu <bineet.s.naidu@gmail.com>
2021-05-19 18:04:30 +0530

    🚀add chakra.ui react

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from '@chakra-ui/react';
+
+export default function Home() {
+  return (
+    <Container maxW="container.xl">
+      <h1>Hello Windbnb</h1>
+    </Container>
+  );
+}
