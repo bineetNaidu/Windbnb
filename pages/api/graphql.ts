2d622d854307b66ddb9d84c0d1c8407dff09eefc
bineetNaidu <bineet.s.naidu@gmail.com>
2022-06-26 14:12:30 +0530

    🎉 add graphql server micro

---
+++ pages/api/graphql.ts
@@ -0,0 +1,28 @@
+import 'reflect-metadata';
+import { ApolloServer } from 'apollo-server-micro';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { buildSchema } from 'type-graphql';
+
+const schema = await buildSchema({
+  resolvers: [],
+});
+
+const server = new ApolloServer({
+  schema,
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const startServer = server.start();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await startServer;
+  await server.createHandler({ path: '/api/graphql' })(req, res);
+}
