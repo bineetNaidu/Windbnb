c5ba4b04b46ca7246ebe0c99369f3a5e6687d5da
bineetNaidu <bineet.s.naidu@gmail.com>
2022-06-26 17:27:56 +0530

    🚀 add providers for request clients

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0';
+import { Hydrate, QueryClientProvider } from 'react-query';
+import { queryClient } from '../lib/requestClients';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <UserProvider>
+          <Component {...pageProps} />
+        </UserProvider>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,26 @@
-import type { NextPage } from 'next';
 import Layout from '../components/Layout';
+import type { NextPage } from 'next';
+import { dehydrate, useQuery } from 'react-query';
+import { getRooms, queryClient } from '../lib/requestClients';
 
 const Home: NextPage = () => {
+  const { data } = useQuery(['rooms'], () => getRooms());
+
   return (
     <Layout>
       <h1>Welcome to WindBnB</h1>
     </Layout>
   );
 };
+
+export async function getServerSideProps() {
+  await queryClient.prefetchQuery(['rooms'], () => getRooms());
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
 
 export default Home;
